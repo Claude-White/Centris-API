0cece252f83ba499661300ca5eda0e10c5c35dea
Pierre Badra <BADRA.PIERRE@cegep-heritage.qc.ca>
2024-12-30 18:45:08 -0500

    temp commit

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -9,10 +9,7 @@ import (
 func (s *Server) RegisterRoutes() http.Handler {
 	mux := http.NewServeMux()
 
-	// Register routes
-	mux.HandleFunc("/", s.HelloWorldHandler)
-
-	mux.HandleFunc("/health", s.healthHandler)
+	mux.HandleFunc("GET /properties/{mls}", s.getProperty)
 	mux.HandleFunc("/brokers", s.GetBrokers)
 
 	// Wrap the mux with CORS middleware
@@ -51,16 +48,9 @@ func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
-	resp, err := json.Marshal(s.db.Health())
-	if err != nil {
-		http.Error(w, "Failed to marshal health check response", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	if _, err := w.Write(resp); err != nil {
-		log.Printf("Failed to write response: %v", err)
-	}
+func (s *Server) getProperty(w http.ResponseWriter, r *http.Request) {
+	mls := r.PathValue("mls")
+
 }
 
 func (s *Server) GetBrokers(w http.ResponseWriter, r *http.Request) {
