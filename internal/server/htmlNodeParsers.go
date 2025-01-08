6a710e30a3b860ebbeff5cecdafb397dc816f3f6
Pierre Badra <BADRA.PIERRE@cegep-heritage.qc.ca>
2025-01-07 21:30:16 -0500

    added more helper functions

--- internal/server/htmlNodeParsers.go
+++ internal/server/htmlNodeParsers.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"log"
+	"net/http"
+	"strconv"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -159,4 +162,49 @@ func findElementsByAttribute(n *html.Node, attrName, attrValue string) []*html.N
 	}
 
 	return matches
+}
+
+func generateSession(url string) (string, string, int) {
+	// Make the GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Failed to make GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Extract cookies from the response headers
+	cookies := resp.Header["Set-Cookie"]
+
+	// Variables to store the desired cookies
+	var aspNetCoreSession string
+	var arrAffinitySameSite string
+
+	// Iterate through cookies to find the ones we need
+	for _, cookie := range cookies {
+		if strings.Contains(cookie, ".AspNetCore.Session=") {
+			aspNetCoreSession = extractCookieValue(cookie, ".AspNetCore.Session")
+		} else if strings.Contains(cookie, "ARRAffinitySameSite=") {
+			arrAffinitySameSite = extractCookieValue(cookie, "ARRAffinitySameSite")
+		}
+	}
+
+	numberOfBrokers := 0
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return aspNetCoreSession, arrAffinitySameSite, numberOfBrokers
+	}
+
+	numberOfBrokers, _ = strconv.Atoi(strings.ReplaceAll(strings.ReplaceAll(findElementByClass(doc, "span", "resultCount"), "\u00a0", ""), " ", ""))
+
+	return aspNetCoreSession, arrAffinitySameSite, numberOfBrokers
+}
+
+func extractCookieValue(cookie string, key string) string {
+	parts := strings.Split(cookie, ";")
+	for _, part := range parts {
+		if strings.HasPrefix(part, key+"=") {
+			return strings.TrimPrefix(part, key+"=")
+		}
+	}
+	return ""
 }
