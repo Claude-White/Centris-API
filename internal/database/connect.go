d49c8a0dd160844bf7fec9c38f232ad26368e245
pierre-badra <PierreBadra@outlook.com>
2025-03-17 00:12:08 -0400

    Added MongoDB database connection file/folder structure

---
+++ internal/database/connect.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func ConnectToDatabase() (*mongo.Database, error) {
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			return
+		}
+	}()
+	return client.Database(os.Getenv("MONGODB_NAME")), nil
+}
