c0da39f1dab1115ff7b3008436ae9f3b4b4e220b
pierre-badra <PierreBadra@outlook.com>
2025-03-18 00:59:06 -0400

    Added Tor Socks client  helper functions

--- internal/server/helper.go
+++ internal/server/helper.go
@@ -3,6 +3,13 @@ package server
 import (
 	"centris-api/internal/repository"
 	"context"
+	"net"
+	"net/http"
+	"os"
+	"runtime"
+	"time"
+
+	"golang.org/x/net/proxy"
 )
 
 func GetCompleteBroker(s *Server, ctx context.Context, broker repository.Broker) (CompleteBroker, error) {
@@ -23,4 +30,47 @@ func GetCompleteBroker(s *Server, ctx context.Context, broker repository.Broker)
 	completeBroker.Broker_Links = broker_links
 
 	return completeBroker, nil
+}
+
+type DialContext func(ctx context.Context, network, addr string) (net.Conn, error)
+
+func NewClientFromEnv() (*http.Client, error) {
+	proxyHost := os.Getenv("PROXY_HOST")
+
+	baseDialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	var dialContext DialContext
+
+	if proxyHost != "" {
+		dialSocksProxy, err := proxy.SOCKS5("tcp", proxyHost, nil, baseDialer)
+		if err != nil {
+			return nil, err
+		}
+		if contextDialer, ok := dialSocksProxy.(proxy.ContextDialer); ok {
+			dialContext = contextDialer.DialContext
+		} else {
+			return nil, err
+		}
+	} else {
+		dialContext = (baseDialer).DialContext
+	}
+
+	httpClient := newClient(dialContext)
+	return httpClient, nil
+}
+
+func newClient(dialContext DialContext) *http.Client {
+	return &http.Client{
+		Transport: &http.Transport{
+			Proxy:                 http.ProxyFromEnvironment,
+			DialContext:           dialContext,
+			MaxIdleConns:          400,
+			IdleConnTimeout:       120 * time.Second,
+			TLSHandshakeTimeout:   30 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			MaxIdleConnsPerHost:   runtime.GOMAXPROCS(0) + 1,
+		},
+	}
 }
